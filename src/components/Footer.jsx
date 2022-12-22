67e407a50f47c2a29a2f998256ad1bc3ce30f5d4
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-22 13:07:12 -0300

    deployment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="py-10">
+        <p className="text-center">APV - Administrador de pacientes de 
+        <span className="text-indigo-600"> Veterinaria</span></p>
+      </footer>
+    )
+  }
+  
+  export default Footer
