f27edc74577f78185e5c571b7c01c534804f7afc
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-22 21:36:27 -0300

    Fixing email isues

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -33,12 +33,12 @@ const ConfirmarCuenta = () => {
     }, [])
     return (
       <>
-           <div>
+          <div>
             <h1 className="text-indigo-600 font-black text-6xl">
                 Confirma tu cuenta y comienza a administrar
                 <span className="text-black"> tus pacientes</span>
             </h1>
-        </div>
+          </div>
             <div  className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
               {!cargando && <Alerta 
               alerta={alerta}/>} 
